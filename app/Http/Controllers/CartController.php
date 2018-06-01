226b73a6eb87ab4edb57fcdffefd83ca7832c962
abhishek47 <operations@trumpetstechnologies.com>
2018-06-01 11:49:32 +0530

    rating

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -56,6 +56,9 @@ class CartController extends Controller
         $combinations = $this->combinations($all_products);
 
 
+        dd($combinations);
+
+        
         $this->generateCarts($combinations);
         
 
