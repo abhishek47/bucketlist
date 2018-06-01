d448fafc21a4c25e8947622d3af17da33d539d1b
abhishek47 <operations@trumpetstechnologies.com>
2018-06-01 08:39:20 +0530

    rating

--- app/Http/Controllers/BudgetController.php
+++ app/Http/Controllers/BudgetController.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 
 class BudgetController extends Controller
 {
+	/**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
 	public function index()
 	{
